ce7030dd847d8c8bd5aca49ae45150d6bd58c3e0
Cristiano <20cristianogoncalves20.22@gmail.com>
2026-09-25 17:10:40 -0300

    chore: initial project structure with spring boot 3

---
+++ src/main/java/com/vendaagil/vendaagil_api/VendaagilApiApplication.java
@@ -0,0 +1,13 @@
+package com.vendaagil.vendaagil_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaagilApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaagilApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vendaagil/vendaagil_api/VendaagilApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendaagil.vendaagil_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaagilApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
